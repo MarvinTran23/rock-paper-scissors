35bfea602558cafeafc9830b6606106855f54147
Marvin Tran <marvintran59@gmail.com>
2026-06-27 17:54:33 +0200

    feat: set up backend structure with Spring Boot and initial configurations

---
+++ backend/src/main/java/com/myproject/rock_paper_scissor_game/RockPaperScissorGameApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.rock_paper_scissor_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/myproject/rock_paper_scissor_game/RockPaperScissorGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.rock_paper_scissor_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
