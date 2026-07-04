b2bab3f7641b0f4e6f1bdac0fa886fef2e631100
Marvin Tran <marvintran59@gmail.com>
2026-07-04 13:55:11 +0200

    add select state

--- frontend/src/components/actionButton.tsx
+++ frontend/src/components/actionButton.tsx
@@ -2,19 +2,31 @@ type ActionButtonProps = {
     text: string;
     onClick?: () => void;
     disabled?: boolean;
+    selected?: boolean;
 }
 
-function ActionButton({ text, onClick, disabled }: ActionButtonProps) {
+
+
+
+function ActionButton({ text, onClick, disabled, selected }: ActionButtonProps) {
     return (
         <button
             disabled={disabled}
             onClick={onClick}
-            className={`text-6xl px-6 py-3 mx-4 border-black rounded-lg transition
-            ${disabled
-                    ? "opacity-50 cursor-not-allowed"
-                    : "hover:bg-black hover:text-white"
-                }`}
-        >
+            className={`
+                text-6xl px-6 py-3 mx-4 rounded-xl
+                border border-gray-300
+                bg-white
+                shadow-md
+                transition-all duration-150
+
+                hover:shadow-lg
+                active:translate-y-1
+
+
+                ${selected ? "rainbow-border" : ""}
+                ${disabled ? "opacity-40 cursor-not-allowed" : ""}
+                `}>
             {text}
         </button>
     );
